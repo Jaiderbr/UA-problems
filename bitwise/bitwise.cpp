009d65dffe57aaed51e0d289ae07ed554f04ae5e
Jaider Bautista Rodriguez <117297163+Jaiderbr@users.noreply.github.com>
2026-04-13 21:17:00 -0500

    ㅤㅤ

---
+++ bitwise/bitwise.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl     '\n' 
+#define f        first
+#define s        second
+#define ins      insert
+#define pb       push_back
+#define eb       emplace_back
+#define sz(x)    int((x).size())
+#define all(x)   begin(x), end(x)
+typedef long long ll;
+typedef unsigned long long ull;
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define each(i, x) for (auto &&i : x)
+#define forne(i,x,n) for (int i = x; i < n; ++i)
+#define show(x) for (auto &&i : x) {cerr << i <<' ';} cerr<< endl;
+
+
+void dbg_out() { cerr << ']' << endl; }
+template<typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) { cerr << H;if (sizeof...(T)) cerr << ',' << ' '; dbg_out(T...); }
+#ifdef LOCAL 
+#define dbg(...) cerr << '|' << __LINE__ << '|'<< '{' << #__VA_ARGS__ << '}'<<':'<<' '<<'[', dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define int int64_t
+
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int test; cin >> test;
+    forn(i, test) {
+        string a, b; cin >> a >> b;
+        string Or = "";
+        string And = "";
+        forn(i, sz(a)) {
+            Or.pb(char('0' + (int(a[i] - '0') | int(b[i] - '0'))));
+            And.pb(char('0' + (int(a[i] - '0') & int(b[i] - '0'))));
+        }
+        cout << And << " " << Or << endl;
+    }
+
+
+    cout << flush;
+    return 0;
+}
